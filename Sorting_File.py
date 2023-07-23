058da87368c28bedcc0f912d06446f06f4e972a2
Taupik-001 <taufikbjm24@gmail.com>
2023-07-23 17:27:08 +0800

    Folder work moved

--- Sorting_File.py
+++ Sorting_File.py
@@ -26,80 +26,86 @@ def move_file_with_rename(source, destination):
 
 
 def categorize_file(file_path, source_directory):
-    # Check if the file is directly inside the specified source_directory or its subdirectories
-    if os.path.commonprefix([file_path, source_directory]) == source_directory:
-        # The file is inside the source directory or its subdirectories, proceed with categorization
-        if os.path.isfile(file_path):
-            # Rest of the categorization and moving process remains the same...
-            if file_path.lower().endswith(
-                (".jpg", ".png", ".jpeg", ".gif", ".webp", ".ico", ".svg")
-            ):
-                create_directory_if_not_exists("D:\\Pictures")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Pictures", os.path.basename(file_path))
-                )
-            elif file_path.lower().endswith(
-                (".doc", ".docx", ".ppt", ".pptx", ".pdf", ".xls", ".xlsx")
-            ):
-                create_directory_if_not_exists("D:\\Documents")
-                move_file_with_rename(
-                    file_path,
-                    os.path.join("D:\\Documents", os.path.basename(file_path)),
-                )
-            elif file_path.lower().endswith((".mp4", ".mkv", ".avi", ".mov")):
-                create_directory_if_not_exists("D:\\Videos")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Videos", os.path.basename(file_path))
-                )
-            elif file_path.lower().endswith((".zip", ".rar", ".7z")):
-                create_directory_if_not_exists("D:\\Compressed")
-                move_file_with_rename(
-                    file_path,
-                    os.path.join("D:\\Compressed", os.path.basename(file_path)),
-                )
-            elif file_path.lower().endswith(
-                (".php", ".html", ".txt", ".css", ".js", ".java", ".py", ".cpp", ".sql")
-            ):
-                create_directory_if_not_exists("D:\\Code")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Code", os.path.basename(file_path))
-                )
-            elif file_path.lower().endswith((".exe", ".app")):
-                create_directory_if_not_exists("D:\\Apps")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Apps", os.path.basename(file_path))
-                )
-            elif file_path.lower().endswith((".mp3", ".wav", ".flac", ".ogg", ".m4a")):
-                create_directory_if_not_exists("D:\\Sounds")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Sounds", os.path.basename(file_path))
-                )
-            else:
-                create_directory_if_not_exists("D:\\Others")
-                move_file_with_rename(
-                    file_path, os.path.join("D:\\Others", os.path.basename(file_path))
-                )
-        elif os.path.isdir(file_path):
-            create_directory_if_not_exists("D:\\Folder")
-            move_file_with_rename(
-                file_path, os.path.join("D:\\Folder", os.path.basename(file_path))
-            )
+    # Rest of the categorization and moving process remains the same...
+    if file_path.lower().endswith(
+        (".jpg", ".png", ".jpeg", ".gif", ".webp", ".ico", ".svg")
+    ):
+        create_directory_if_not_exists("D:\\Pictures")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Pictures", os.path.basename(file_path))
+        )
+    elif file_path.lower().endswith(
+        (".doc", ".docx", ".ppt", ".pptx", ".pdf", ".xls", ".xlsx")
+    ):
+        create_directory_if_not_exists("D:\\Documents")
+        move_file_with_rename(
+            file_path,
+            os.path.join("D:\\Documents", os.path.basename(file_path)),
+        )
+    elif file_path.lower().endswith((".mp4", ".mkv", ".avi", ".mov")):
+        create_directory_if_not_exists("D:\\Videos")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Videos", os.path.basename(file_path))
+        )
+    elif file_path.lower().endswith((".zip", ".rar", ".7z")):
+        create_directory_if_not_exists("D:\\Compressed")
+        move_file_with_rename(
+            file_path,
+            os.path.join("D:\\Compressed", os.path.basename(file_path)),
+        )
+    elif file_path.lower().endswith(
+        (".php", ".html", ".txt", ".css", ".js", ".java", ".py", ".cpp", ".sql")
+    ):
+        create_directory_if_not_exists("D:\\Code")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Code", os.path.basename(file_path))
+        )
+    elif file_path.lower().endswith((".exe", ".app")):
+        create_directory_if_not_exists("D:\\Apps")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Apps", os.path.basename(file_path))
+        )
+    elif file_path.lower().endswith((".mp3", ".wav", ".flac", ".ogg", ".m4a")):
+        create_directory_if_not_exists("D:\\Sounds")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Sounds", os.path.basename(file_path))
+        )
+    else:
+        create_directory_if_not_exists("D:\\Others")
+        move_file_with_rename(
+            file_path, os.path.join("D:\\Others", os.path.basename(file_path))
+        )
 
+
+def move_folder_to_sorted_directory(folder_path, target_directory):
+    folder_name = os.path.basename(folder_path)
+    target_path = os.path.join(target_directory, folder_name)
+    if not os.path.exists(target_path):
+        shutil.move(folder_path, target_directory)
+        print(f"Folder '{folder_name}' moved to '{target_directory}'.")
 
 if __name__ == "__main__":
     # Specify the source directory to monitor
     source_directory = "D:\\_Sorting-File\\"
+    sorted_folders_directory = "D:\\Folder\\"
 
-    # Infinite loop to continuously categorize files with a 1-second delay between iterations
+    # Infinite loop to continuously categorize files and directories with a 1-second delay between iterations
     while True:
-        files_found = False  # Flag to check if any files or folders are found
-        for root, _, files in os.walk(source_directory):
-            for file in files:
-                file_path = os.path.join(root, file)
-                categorize_file(file_path, source_directory)
-                files_found = (
-                    True  # Set the flag to True if any files or folders are found
-                )
-        if not files_found:
+        items_found = False  # Flag to check if any files or folders are found
+
+        for root, dirs, items in os.walk(source_directory):
+            # Move and sort the folders first
+            for folder in dirs:
+                folder_path = os.path.join(root, folder)
+                move_folder_to_sorted_directory(folder_path, sorted_folders_directory)
+                items_found = True  # Set the flag to True if any files or folders are found
+
+            # Categorize and move the files
+            for item in items:
+                item_path = os.path.join(root, item)
+                categorize_file(item_path, source_directory)
+                items_found = True  # Set the flag to True if any files or folders are found
+
+        if not items_found:
             print("No file or folder was found in the specified directory.")
         time.sleep(1)  # Wait for 1 second before the next iteration
